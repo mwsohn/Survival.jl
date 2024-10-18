d79c9744743ca104e3bafc7d917149f904c3d7af
mwsohn <mwsohn@gmail.com>
2024-10-18 15:51:46 -0400

    Update kaplanmeier.jl

--- src/kaplanmeier.jl
+++ src/kaplanmeier.jl
@@ -69,7 +69,7 @@ StatsAPI.fit(::Type{KaplanMeier}, ets)
 
 Plots Kaplan-Meier estimates.
 """
-function kaplanmeier(df, event::EventTime; by::Symbol=nothing)
+function kaplanmeier(df, event::EventTime; by=nothing)
 
     plt = nothing
     if by == nothing
